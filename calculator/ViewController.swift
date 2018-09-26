115ace49336f6937a7fdd04b3918dc932c888ee1
Andy Chen <chenrui@chenruitekiMacBook-Air.local>
2018-09-26 19:52:56 +0800

    changedcode

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func show(_ sender: Any) {
+        sum.text = equal
+    }
+    @IBOutlet weak var sum: UILabel!
+    var equal:String = "0"
+    @IBAction func addequal(_ sender: Any) {
+        equal = "\(Int(input1.text!)! + Int(input2.text!)!)"
+    }
+    @IBOutlet weak var result: UITextField!
+    @IBOutlet weak var input2: UITextField!
+    @IBOutlet weak var input1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
