7ad2fa28d01f74a93a6f1c273669232f0af09e2e
Andy Chen <chenrui@chenruitekiMacBook-Air.local>
2019-01-06 09:51:10 +0800

    half finished

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,72 +10,205 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBAction func zero(_ sender: Any) {
-        sum.text = "0"
-        a = "0"
-    }
-    @IBAction func nine(_ sender: Any) {
-        sum.text = "9"
-        a = "9"
-    }
-    @IBAction func eight(_ sender: Any) {
-        sum.text = "8"
-        a = "8"
-    }
-    @IBAction func seven(_ sender: Any) {
-        sum.text = "7"
-        a = "7"
-    }
-    @IBAction func six(_ sender: Any) {
-        sum.text = "6"
-        a = "6"
-    }
-    @IBAction func five(_ sender: Any) {
-        sum.text = "5"
-        a = "5"
-    }
-    @IBAction func four(_ sender: Any) {
-        sum.text = "4"
-        a = "4"
-    }
-    @IBAction func three(_ sender: Any) {
-        sum.text = "3"
-        a = "3"
-    }
-    @IBAction func two(_ sender: Any) {
-        sum.text = "2"
-        a = "2"
-    }
-    @IBAction func one(_ sender: Any) {
-        sum.text = "1"
-        a = "1"
-    }
     var sw:Bool = false
     var end = "0"
-    var a = "0"
-    var ch = "0"
+    var endd = "0"
+    var finish = "0"
+    var p:Bool = false//是否清空
+    //var point:Bool = false//判断是否有小数点
+    var a = "0"//数字
+    var ch = "0"//符号
+    
+    @IBAction func yushu(_ sender: Any) {
+        sw = true
+        sum.text = sum.text! + "%"
+        ch = "%"
+    }
+    
+    @IBAction func point(_ sender: Any) {
+        sum.text = sum.text! + "."
+        a = "."
+        //point = true
+        end += a
+    }
+    
+    @IBAction func zero(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "0"
+        a = "0"
+        end += a
+    }
+    @IBAction func nine(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "9"
+        a = "9"
+        end += a
+    }
+    @IBAction func eight(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "8"
+        a = "8"
+        end += a
+    }
+    @IBAction func seven(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "7"
+        a = "7"
+        end += a
+    }
+    @IBAction func six(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "6"
+        a = "6"
+        end += a
+    }
+    @IBAction func five(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "5"
+        a = "5"
+        end += a
+    }
+    @IBAction func four(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "4"
+        a = "4"
+        end += a
+    }
+    @IBAction func three(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "3"
+        a = "3"
+        end += a
+    }
+    @IBAction func two(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "2"
+        a = "2"
+        end += a
+    }
+    @IBAction func one(_ sender: Any) {
+        if p {
+            sum.text = ""
+            p = false
+        }
+        sum.text = sum.text! + "1"
+        a = "1"
+        end += a
+    }
+    
+    @IBAction func qingkong(_ sender: Any) {
+        sum.text = ""
+        end = "0"
+        endd = "0"
+    }
     @IBAction func show(_ sender: Any) {
         //sum.text = equal
+        
         if ch == "+" {
-            while(sw) {
-                end += "Int(ch!)!"
-                sum.text = end
+            if(sw) {
+                finish = "\(Double(end)! + Double(endd)!)"
             }
         }
+        if ch == "*" {
+            if(sw) {
+                finish = "\(Double(end)! * Double(endd)!)"
+            }
+        }
+        if ch == "-" {
+            if(sw) {
+                finish = "\(Double(end)! - Double(endd)!)"
+            }
+        }
+        if ch == "/" {
+            if(sw) {
+                finish = "\(Double(end)! / Double(endd)!)"
+            }
+        }
+        if ch == "%" {
+            if(sw) {
+                finish = "\(Int(end)! % Int(endd)!)"
+            }
+        }
+        sw = false
+        let _:Double = Double(finish)!
+        finish = String(format:"%.8f",Double(finish)!)
+        while (finish.last == "0"){
+            finish.removeLast()
+        }
+        if (finish.last == "."){
+            finish.removeLast()
+        }
         
+        if sum.text == "0" {
+            sum.text = "错误"
+        }
+        else {
+            sum.text = finish
+            p = true
+        }
+        
+        end = "0"
+        endd = "0"
     }
     @IBOutlet weak var sum: UILabel!
     var equal:String = "0"
     @IBAction func addequal(_ sender: Any) {
         //equal = "\(Int(input1.text!)! + Int(input2.text!)!)"
         sw = true
-        sum.text = "+"
+        endd = end
+        end = "0"
+        sum.text = sum.text! + "+"
         ch = "+"
     }
-    @IBOutlet weak var result: UITextField!
-    @IBOutlet weak var input2: UITextField!
-    @IBOutlet weak var input1: UITextField!
+    
+    @IBAction func chu(_ sender: Any) {
+        sw = true
+        endd = end
+        end = "0"
+        sum.text = sum.text! + "/"
+        ch = "/"
+    }
+    @IBAction func reduce(_ sender: Any) {
+        sw = true
+        endd = end
+        end = "0"
+        sum.text = sum.text! + "-"
+        ch = "-"
+    }
+    @IBAction func cheng(_ sender: Any) {
+        sw = true
+        endd = end
+        end = "0"
+        sum.text = sum.text! + "*"
+        ch = "*"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
